cad9691f62f974746a522c9d1bea724e31711e40
genevaja <genevaja@student.21-school.ru>
2022-08-17 20:12:42 +0300

    Остановился здесь

--- src/tests/s21_negate_test.c
+++ src/tests/s21_negate_test.c
@@ -1,116 +1,32 @@
-
 #include "../header.h"
 
-START_TEST(negate_1) {
+START_TEST(add_1) {
     char s21_result[BUF];
     char result[BUF];
     memset(result, '\0', BUF);
     memset(s21_result, '\0', BUF);
-    int a = INT_MAX; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
+    int a = 421, b = 79; 
+    s21_decimal number1 = {0};
+    s21_decimal number2 = {0};
+    s21_from_int_to_decimal(a, &number1);
+    s21_from_int_to_decimal(a, &number2);
     s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
+    s21_add(number1, number2, &dec_result);
     dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
-    ck_assert_str_eq(result, s21_result);
-}
-END_TEST
-
-START_TEST(negate_2) {
-    char s21_result[BUF];
-    char result[BUF];
-    memset(result, '\0', BUF);
-    memset(s21_result, '\0', BUF);
-    int a = INT_MIN + 1; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
-    s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
-    dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
-    ck_assert_str_eq(result, s21_result);
-}
-END_TEST
-
-START_TEST(negate_3) {
-    char s21_result[BUF];
-    char result[BUF];
-    memset(result, '\0', BUF);
-    memset(s21_result, '\0', BUF);
-    int a = 1059308; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
-    s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
-    dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
-    ck_assert_str_eq(result, s21_result);
-}
-END_TEST
-
-START_TEST(negate_4) {
-    char s21_result[BUF];
-    char result[BUF];
-    memset(result, '\0', BUF);
-    memset(s21_result, '\0', BUF);
-    int a = -10530508; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
-    s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
-    dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
-    ck_assert_str_eq(result, s21_result);
-}
-END_TEST
-
-START_TEST(negate_5) {
-    char s21_result[BUF];
-    char result[BUF];
-    memset(result, '\0', BUF);
-    memset(s21_result, '\0', BUF);
-    int a = 0; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
-    s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
-    dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
-    ck_assert_str_eq(result, s21_result);
-}
-END_TEST
-
-START_TEST(negate_6) {
-    char s21_result[BUF];
-    char result[BUF];
-    memset(result, '\0', BUF);
-    memset(s21_result, '\0', BUF);
-    int a = -0; 
-    s21_decimal number = {0};
-    s21_from_int_to_decimal(a, &number);
-    s21_decimal dec_result = {0};
-    s21_negate(number, &dec_result);
-    dec_output(&dec_result, s21_result);
-    sprintf(result, "%d", a * (-1));
+    sprintf(result, "%d", a + b);
     ck_assert_str_eq(result, s21_result);
 }
 END_TEST
 
 Suite * sprintf_test(void) {
     Suite *s;
-    TCase *tc_negate;
+    TCase *tc_add;
 
-    s = suite_create("s21_negate");
+    s = suite_create("s21_add");
 
-    tc_negate = tcase_create("s21_negate");
-    tcase_add_test(tc_negate, negate_1);
-    tcase_add_test(tc_negate, negate_2);
-    tcase_add_test(tc_negate, negate_3);
-    tcase_add_test(tc_negate, negate_4);
-    tcase_add_test(tc_negate, negate_5);
-    tcase_add_test(tc_negate, negate_6);
-    suite_add_tcase(s, tc_negate);
+    tc_add = tcase_create("s21_add");
+    tcase_add_test(tc_add, add_1);
+    suite_add_tcase(s, tc_add);
 
     return s;
 }
