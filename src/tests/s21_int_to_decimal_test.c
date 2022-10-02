01b3369379c972f1d08808998512023124b1ddec
Albert <genevaja@student.21-school.ru>
2022-10-02 16:36:08 +0300

    Добавил цель в makefile для valgrind

--- src/tests/s21_int_to_decimal_test.c
+++ src/tests/s21_int_to_decimal_test.c
@@ -84,6 +84,216 @@ START_TEST(int_to_decimal_6) {
 }
 END_TEST
 
+START_TEST(int_to_decimal_7) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 299928579; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_8) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 1; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_9) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -1; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_10) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 1200; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_11) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -1200; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_12) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 1200002982; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_13) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -1200002982; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_14) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -2002987; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_15) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 2002987; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_16) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 1211171; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_17) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -1211171; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_18) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 8571902; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_19) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -8571902; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_20) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = 62771781; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
+START_TEST(int_to_decimal_21) {
+    char s21_result[BUF];
+    char result[BUF];
+    memset(result, '\0', BUF);
+    memset(s21_result, '\0', BUF);
+    int a = -62771781; 
+    s21_decimal number = {0};
+    s21_from_int_to_decimal(a, &number);
+    dec_to_string(&number, s21_result);
+    sprintf(result, "%d", a);
+    ck_assert_str_eq(result, s21_result);
+}
+END_TEST
+
 Suite * sprintf_test(void) {
     Suite *s;
     TCase *tc_int_to_decimal;
@@ -97,6 +307,21 @@ Suite * sprintf_test(void) {
     tcase_add_test(tc_int_to_decimal, int_to_decimal_4);
     tcase_add_test(tc_int_to_decimal, int_to_decimal_5);
     tcase_add_test(tc_int_to_decimal, int_to_decimal_6);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_7);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_8);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_9);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_10);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_11);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_12);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_13);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_14);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_15);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_16);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_17);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_18);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_19);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_20);
+    tcase_add_test(tc_int_to_decimal, int_to_decimal_21);
     suite_add_tcase(s, tc_int_to_decimal);
 
     return s;
